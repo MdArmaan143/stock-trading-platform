32b85dd57d27d6d177ae503d315c7dbd3a79cc4b
Md Armaan <armaanshaikhh786@gmail.com>
2024-09-06 12:52:33 +0530

    education and pricing section done

--- src/landing_page/home/Education.jsx
+++ src/landing_page/home/Education.jsx
@@ -2,7 +2,23 @@ import React from 'react'
 
 function Education() {
     return ( 
-        <h1>Education</h1>
+        <div className='container mt-5 mb-5'>
+        <div className='row'>
+          <div className='col-6 p-5'>
+          <img src='media\images\education.svg' alt=' education Image' style={{width:"80%"}}/>
+          </div>
+          <div className='col-6 p-5 mt-5'>
+            <h1 className='fs-2'>Free and open market education</h1>
+            <p className='mt-5'>Varsity, the largest online stock market education book in the world covering everything from the basics to advanced trading. </p>
+            <a href='#'style={{textDecoration:"none"}} >See pricing  <i class="fas fa-arrow-right"></i></a>
+           
+            <p className='mt-5'>TradingQ&A, the most active trading and investment community in India for all your market related queries. </p>
+            <a href='#'style={{textDecoration:"none"}} >TradingQ&A   <i class="fas fa-arrow-right"></i></a>
+        
+        
+          </div>
+          </div>  
+      </div>
      );
 }
 
--- src/landing_page/home/Pricing.jsx
+++ src/landing_page/home/Pricing.jsx
@@ -2,9 +2,32 @@ import React from 'react'
 
 function Pricing() {
     return ( 
-        <h1>
-            Pricing
-        </h1>
+       <div className='container'>
+        <div className='row'>
+            <div className='col-4'>
+                <h1 className='fs-2 mb-3'>Unbeatable pricing</h1>
+                <p>We pioneered the concept of discount broking and price transparency in India. Flat fees and no hidden charges.</p>
+                <a href='#'style={{textDecoration:"none"}} >See pricing  <i class="fas fa-arrow-right"></i></a>
+            </div>
+            <div className='col-2'></div>
+            <div className='col-6'>
+                <div className='row text-center'>
+                    <div className='col p-3 border'>
+                        <h1 className='mb-3'><i class="fa-solid fa-indian-rupee-sign"></i>0</h1>
+                        <p>Free equity delivery
+                        and<br></br> direct mutual funds</p>
+                    </div>
+                    <div className='col p-3 border'>
+                    <h1 className='mb-3'><i class="fa-solid fa-indian-rupee-sign"></i>20</h1>
+                    <p>Intraday and
+                    F&O</p>
+                    </div>
+                  
+                </div>
+
+            </div>
+        </div>
+       </div>
      );
 }
 
